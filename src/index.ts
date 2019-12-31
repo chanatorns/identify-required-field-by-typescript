32884868861e2358802248da2adf155f0205ed7c
chanatorns <chanatorns@gmail.com>
2020-01-01 00:53:38 +0700

    update code

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,39 @@
 interface RequiredItemField {
-  id: string,
-  media: string[],
+  id: string;
+  media: string[];
   variant: {
-    vid: string
-  }[]
+    vid: string;
+  }[];
 }
 interface DecoratedField {
-  newField2: string
+  newField2: string;
 }
 const mutateFunc = (aaa: RequiredItemField) => {
-  let decorateData: Partial<DecoratedField & RequiredItemField['variant'][0]> = {};
+  let decorateData: Partial<
+    DecoratedField & RequiredItemField["variant"][0]
+  > = {};
 
   if (aaa.variant.length > 0) {
     decorateData = aaa.variant[0];
   }
 
-  decorateData.newField2 = 'nnn';
+  decorateData.newField2 = "nnn";
   delete aaa.media;
 
   return { ...aaa, ...decorateData };
-}
+};
 
 interface Item {
-  id: string,
-  media: string[],
+  id: string;
+  media: string[];
   variant: {
-    vid: string
-  }[],
-  price: number
+    vid: string;
+  }[];
+  price: number;
 }
 const main = (item: Item) => {
-  const mutated = mutateFunc(item)
-  console.log(mutated)
-}
+  const mutated = mutateFunc(item);
+  console.log(mutated);
+};
 
-main({a: 'what'});
+main({ a: "what" } as any);
