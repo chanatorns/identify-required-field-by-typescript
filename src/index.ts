a7459d2623401155792367343cb92c909c554a14
chanatorns <chanatorns@gmail.com>
2020-01-01 00:50:34 +0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+interface RequiredItemField {
+  id: string,
+  media: string[],
+  variant: {
+    vid: string
+  }[]
+}
+interface DecoratedField {
+  newField2: string
+}
+const mutateFunc = (aaa: RequiredItemField) => {
+  let decorateData: Partial<DecoratedField & RequiredItemField['variant'][0]> = {};
+
+  if (aaa.variant.length > 0) {
+    decorateData = aaa.variant[0];
+  }
+
+  decorateData.newField2 = 'nnn';
+  delete aaa.media;
+
+  return { ...aaa, ...decorateData };
+}
+
+interface Item {
+  id: string,
+  media: string[],
+  variant: {
+    vid: string
+  }[],
+  price: number
+}
+const main = (item: Item) => {
+  const mutated = mutateFunc(item)
+  console.log(mutated)
+}
+
+main({a: 'what'});
